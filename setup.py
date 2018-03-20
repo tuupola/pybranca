3cf344b6c76369c189a912ed3b2881e49cd9f630
Mika Tuupola <tuupola@appelsiini.net>
2018-03-20 18:28:36 +0700

    Read long description from README

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
-import sys
-sys.path.pop(0)
 from setuptools import setup
+from codecs import open
+from os import path
+
+cwd = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(cwd, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="pybranca",
     py_modules=["branca", "xchacha20poly1305"],
-    version="0.1.0",
+    version="0.1.2",
     description="Authenticated and encrypted API tokens using modern crypto",
-    long_description="Branca is a secure easy to use token format which makes it hard to shoot yourself in the foot. It uses IETF XChaCha20-Poly1305 AEAD symmetric encryption to create encrypted and tamperproof tokens. The enrypted token is base62 encoded which makes it URL safe. Payload itself is an arbitrary sequence of bytes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     keywords="api, token, jwt, xchacha20, poly1305",
     url="https://github.com/tuupola/branca-python",
     author="Mika Tuupola",
