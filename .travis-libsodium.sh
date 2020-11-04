44e7212c01c3132a995309a0cca49991043cbc4c
Jean-Louis Fuchs <jean-louis.fuchs@adfinis-sygroup.ch>
2020-11-04 18:55:41 +0100

    Fix libsodium download location

--- .travis-libsodium.sh
+++ .travis-libsodium.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 
-wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.16.tar.gz
+wget https://download.libsodium.org/libsodium/releases/old/libsodium-1.0.16.tar.gz
 tar -xzvf libsodium-1.0.16.tar.gz
 cd libsodium-1.0.16
 ./configure --prefix=/usr
