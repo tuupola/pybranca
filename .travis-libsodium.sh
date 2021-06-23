98fbabd104c95f10a2c6ca6949ed8e9a0614af11
Mika Tuupola <tuupola@appelsiini.net>
2021-06-23 15:03:09 +0300

    Remove Travis config

--- .travis-libsodium.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -ex
-
-wget https://download.libsodium.org/libsodium/releases/old/libsodium-1.0.16.tar.gz
-tar -xzvf libsodium-1.0.16.tar.gz
-cd libsodium-1.0.16
-./configure --prefix=/usr
-make && make install
